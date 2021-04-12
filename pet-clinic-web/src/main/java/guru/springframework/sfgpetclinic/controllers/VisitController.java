e9fe98df55c46de46f4a44e8644a8ca6a974efcc
Jacinto J. Mena Lomeña <jacintomena@gmail.com>
2021-04-13 00:31:55 +0200

    Changes to configure date format at annotation level rather than registering a custom editor.

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VisitController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VisitController.java
@@ -11,8 +11,6 @@ import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.beans.PropertyEditorSupport;
-import java.time.LocalDate;
 import java.util.Map;
 
 @Controller
@@ -25,12 +23,6 @@ public class VisitController {
     @InitBinder
     public void setAllowedFields(WebDataBinder dataBinder) {
         dataBinder.setDisallowedFields("id");
-        dataBinder.registerCustomEditor(LocalDate.class, new PropertyEditorSupport() {
-            @Override
-            public void setAsText(String text) throws IllegalArgumentException {
-                setValue(LocalDate.parse(text));
-            }
-        });
     }
 
     @ModelAttribute("visit")
