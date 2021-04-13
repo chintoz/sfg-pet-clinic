6cca886f092f5dfe41caf275ef44e89e56bd21b3
Jacinto J. Mena Lomeña <jacintomena@gmail.com>
2021-04-13 23:48:06 +0200

    Changes to add endpoint to display a JSON with a list of vets. Closes #68.

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -1,9 +1,14 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -19,4 +24,10 @@ public class VetController {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
+
+    @GetMapping(value = {"/api/vets"})
+    public @ResponseBody Set<Vet> showResourcesVetList() {
+        return vetService.findAll();
+    }
+
 }
